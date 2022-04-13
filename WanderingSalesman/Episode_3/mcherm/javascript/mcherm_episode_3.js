48f228d94a8714fc1204d8983958349b4084b801
Michael Chermside <mcherm@mcherm.com>
2022-04-12 22:05:57 -0400

    Create episode 3 for javascript, except it doesn't have bigint support.

---
+++ WanderingSalesman/Episode_3/mcherm/javascript/mcherm_episode_3.js
@@ -0,0 +1,38 @@
+//
+// Implementation of Episode 2 of the Wandering Salesman problem
+//
+
+const fs = require('fs')
+
+const mapData = JSON.parse(fs.readFileSync("../../../inputdata/small-map.json"));
+
+memoCache = {};
+
+function countPaths(startNode, length) {
+    // --- check if it's in the cache ---
+    if (startNode in memoCache && length in memoCache[startNode]) {
+        return memoCache[startNode][length];
+    }
+    // --- work out the answer ---
+    let answer;
+    if (length === 1) {
+        answer = 1;
+    } else {
+        answer = 0;
+        for (const neighbor of mapData.neighbors[startNode]) {
+            answer += countPaths(neighbor, length - 1);
+        }
+    }
+    // --- store the answer and return it ---
+    if (!(startNode in memoCache)) {
+        memoCache[startNode] = {};
+    }
+    memoCache[startNode][length] = answer;
+    return answer
+}
+
+
+const pathLen = 40;
+const pathCount = countPaths("A", pathLen);
+console.log(`For length ${pathLen} there are ${pathCount} paths.`);
+
