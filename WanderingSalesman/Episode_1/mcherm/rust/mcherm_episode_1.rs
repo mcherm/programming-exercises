f8238c09f88ced5a26dbafca33dd22f83e9b6949
Michael Chermside <mcherm@mcherm.com>
2022-04-19 08:14:37 -0400

    Version of episode 1 in rust that doesn't run around creating new strings.

--- WanderingSalesman/Episode_1/mcherm/rust/mcherm_episode_1.rs
+++ WanderingSalesman/Episode_1/mcherm/rust/mcherm_episode_1.rs
@@ -4,25 +4,26 @@ use serde::Deserialize;
 use serde_json;
 
 
-type Node = String;
-type Path = Vec<Node>;
-
 #[derive(Deserialize)]
 struct MapDescription {
-    neighbors: HashMap<Node,Vec<Node>>
+    neighbors: HashMap<String,Vec<String>>
 }
 
 
-fn load_map_description() -> Result<MapDescription, Box<dyn std::error::Error>> {
+fn load_map_description<'a>() -> Result<MapDescription, Box<dyn std::error::Error>> {
     let s = fs::read_to_string("../../../inputdata/small-map.json")?;
     let answer = serde_json::from_str(&s)?;
     return Ok(answer);
 }
 
 
+type Node<'a> = &'a str;
+type Path<'a> = Vec<Node<'a>>;
+
+
 /// Given a map and a start position and number of steps, this returns all
 /// paths of that length starting from that location.
-fn get_paths(map: &MapDescription, start_position: &Node, num_steps: u32) -> Vec<Path> {
+fn get_paths<'a>(map: &'a MapDescription, start_position: Node<'a>, num_steps: u32) -> Vec<Path<'a>> {
     match num_steps {
         1 => vec![vec![start_position.clone()]],
         _ => {
@@ -31,7 +32,7 @@ fn get_paths(map: &MapDescription, start_position: &Node, num_steps: u32) -> Vec
             for neighbor in neighbors {
                 let paths_from_here: Vec<Path> = get_paths(map, neighbor, num_steps - 1);
                 for mut path_from_here in paths_from_here {
-                    path_from_here.insert(0, start_position.to_string());
+                    path_from_here.insert(0, start_position);
                     answer.push(path_from_here);
                 }
             }
@@ -45,7 +46,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let map = load_map_description()?;
 
     let path_len = 14;
-    let paths: Vec<Path> = get_paths(&map, &"A".to_string(), path_len);
+    let paths: Vec<Path> = get_paths(&map, &"A", path_len);
     println!("The paths are: {:?}", paths);
     println!("For length {} there are {} paths.", path_len, paths.len());
     return Ok(());
